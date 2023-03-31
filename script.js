d68af89c3495afa790da47d3a6fc1a29fe83fad3
blthai <39754216+blthai@users.noreply.github.com>
2023-03-30 17:46:02 -0700

    add event listener to squares to toggle class when mouse enters

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container=document.querySelector('div');
-let div=document.createElement('div');
 
 for(i=0;i<256;i++){
   let square=document.createElement('div');
   square.classList.add('square');
   container.appendChild(square);
 }
+
+const squares=document.querySelectorAll('.square');
+
+function colorSquare(event){
+  this.classList.toggle('colored-square');
+  this.removeEventListener('mouseenter',colorSquare);
+}
+
+squares.forEach((square)=>{square.addEventListener('mouseenter',colorSquare)});
 
 
